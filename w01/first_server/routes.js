56330a08484481c85a6338effdc60100d0a27f62
lydiaswan15 <lydiaswan15@gmail.com>
2021-09-16 09:57:51 -0600

    adding the group projects.

--- w01/first_server/routes.js
+++ w01/first_server/routes.js
@@ -1,49 +1,49 @@
-const fs = require('fs'); //fs is a module that allows you to work with the file system. We are importing it right here and assigning it a variable. 
-
+const fs = require("fs"); //fs is a module that allows you to work with the file system. We are importing it right here and assigning it a variable.
 
 const requestHandler = (req, res) => {
+  const url = req.url;
+  const method = req.method;
 
-    const url = req.url;
-    const method = req.method;
+  if (url === "/") {
+    //listening to requests with /
+    res.write("<html>");
+    res.write("<head><title>My First Page</title><head>");
+    res.write(
+      '<body><form action = "/message" method = "POST"><input type = "text" name = "message"><button type = "submit">Send</button></form></body>'
+    );
+    res.write("<html>"); //when send button is clicked, we sent a post request to /message
+    return res.end();
+  }
 
-    if (url === '/') { //listening to requests with /
-        res.write('<html>');
-        res.write('<head><title>My First Page</title><head>');
-        res.write('<body><form action = "/message" method = "POST"><input type = "text" name = "message"><button type = "submit">Send</button></form></body>');
-        res.write('<html>'); //when send button is clicked, we sent a post request to /message
-        return res.end();
-    }
-
-    if (url === '/message' && method === "POST") { //If the url is /message, then it will store the message the user entered and store it in the new file. 
-        const body = [];
-        req.on('data', (chunk) => { //node will keep executing this as long as there is data to add. 
-            console.log(chunk);
-            body.push(chunk);
-        }); //goes to request and creates an event listener. on() automatically does that for us. We are listening for the data event. Data event is fired whenever a new chunk is ready to be read 
-        return req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString(); //creates a new buffer and adds all the chunks from the body array to it and then converts it to a string
-            console.log(parsedBody);
-            const message = parsedBody.split('=')[1];
-            fs.writeFile('message.txt', message, (err) => {
-                res.statusCode = 302;
-                res.setHeader = ('Location', '/');
-                return res.end();
-            });
-
-        }); //will be fired once it's done  parsing the incoming reqest data
-
+  if (url === "/message" && method === "POST") {
+    //If the url is /message, then it will store the message the user entered and store it in the new file.
+    const body = [];
+    req.on("data", (chunk) => {
+      //node will keep executing this as long as there is data to add.
+      console.log(chunk);
+      body.push(chunk);
+    }); //goes to request and creates an event listener. on() automatically does that for us. We are listening for the data event. Data event is fired whenever a new chunk is ready to be read
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString(); //creates a new buffer and adds all the chunks from the body array to it and then converts it to a string
+      console.log(parsedBody);
+      const message = parsedBody.split("=")[1];
+      fs.writeFile("message.txt", message, (err) => {
         res.statusCode = 302;
-        res.setHeader = ('Location', '/');
+        res.setHeader = ("Location", "/");
         return res.end();
-    }
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Page</title><head>');
-    res.write('<body><h1>Hello From My Node.JS Server</h1></body>');
-    res.write('<html>');
-    res.end();
+      });
+    }); //will be fired once it's done  parsing the incoming reqest data
 
-}
-
+    res.statusCode = 302;
+    res.setHeader = ("Location", "/");
+    return res.end();
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title><head>");
+  res.write("<body><h1>Hello From My Node.JS Server</h1></body>");
+  res.write("<html>");
+  res.end();
+};
 
 module.exports = requestHandler; //exports the variable with the function in it so that it can be used in other files
--- w01/w01_assignment/app.js
+++ w01/w01_assignment/app.js
@@ -1,11 +1,13 @@
 const http = require('http');
 
-
+// Create a new server
 const server = http.createServer((req, res) => {
 
     const url = req.url;
     const method = req.method;
 
+    // Openning page
+    // Input entered into the form will submit a POST request to /create-user upon button click
     if (url === '/') {
         res.write('<html>');
         res.write('<head><title>W01 Assignment</title></head>');
@@ -15,6 +17,7 @@ const server = http.createServer((req, res) => {
         return res.end();
     }
 
+    // List of dummy users on url /users
     if (url === '/users') {
         res.write('<html>');
         res.write('<head><title>W01 Assignment</title></head>');
@@ -27,11 +30,26 @@ const server = http.createServer((req, res) => {
         res.write('<li>User 5</li>');
         res.write('</ul>');
         res.write('</html>');
+
     }
 
+    //Using request information (entered by user)
+    // adding to an array
+    // Using a buffer to convert array to a string
+    // logging newly
     if (url === '/create-user' & method === 'POST') {
-
-        return res.end();
+        const usernames = [];
+        req.on('data', (chunk) => {
+            usernames.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedUsernames = Buffer.concat(usernames).toString(); //going through the list and parsing it.
+            const message = parsedUsernames.split('=')[1];
+            console.log(message);
+        })
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        res.end();
     }
 });
 
