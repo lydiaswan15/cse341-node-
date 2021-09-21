c1d9b24ba3b84b2ff9a1025d95fdde828321483f
lydiaswan15 <lydiaswan15@gmail.com>
2021-09-21 09:48:20 -0600

    adding routes

--- w02/first_server/app.js
+++ w02/first_server/app.js
@@ -2,15 +2,18 @@ const http = require('http'); //Importing a file from node.js. Requrie either ta
 const express = require('express');
 
 const app = express();
+const bodyParser = require('body-parser');
 
-app.use('/add-product', (req, res, next) =>{
-    console.log("This always runs!");
-    next();
-});
+const adminRoutes = require('./routes/admin');
+const shopRoutes= require('./routes/shop');
 
-app.use('/', (req, res, next) =>{
-    console.log("IN another middle");
-    res.send('<h1>Hello from Express</h1>');
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send("<h1>Page not found</h1>");
 });
 
 app.listen(3000); //listen starts a process where node.js will keep this running to listen for incoming requests. Will need to pass a port. 
